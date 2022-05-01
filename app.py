4f00869d01c799c045df420b5cd3c4e3a285dd49
flyotlin <flyotlin@gmail.com>
2022-05-01 21:17:16 +0800

    Update new handlers to app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import configparser
 import logging
 import os
 import telegram
@@ -7,22 +8,32 @@ from telegram.ext import Dispatcher
 
 from src.handler.help import HelpHandler
 from src.handler.token import TokenHandler
+from src.handler.check import CheckHandler
+from src.handler.reserve import ReserveHandler
 from src.handler.toggle_poll import TogglePollHandler
+from src.handler.toggle_remind import ToggleRemindHandler
+from src.handler.toggle_reserve import ToggleReserveHandler
 
 # Need to set webhook on
-TOKEN = ""
+config = configparser.ConfigParser()
+config.read('.telegram-bot-token')
+TELEGRAM_BOT_TOKEN = config['bot'].get('token')
 
 # Flask server
 app = Flask(__name__)
 
 # telegram Bot
-bot = telegram.Bot(token=TOKEN)
+bot = telegram.Bot(token=TELEGRAM_BOT_TOKEN)
 
 # telegram Bot dispatchers
 dispatcher = Dispatcher(bot, None)
 dispatcher.add_handler(HelpHandler)
 dispatcher.add_handler(TokenHandler)
+dispatcher.add_handler(CheckHandler)
+dispatcher.add_handler(ReserveHandler)
 dispatcher.add_handler(TogglePollHandler)
+dispatcher.add_handler(ToggleRemindHandler)
+dispatcher.add_handler(ToggleReserveHandler)
 
 # Enable logging
 logging.basicConfig(
@@ -43,6 +54,7 @@ def ping():
     return ("pong", 200)
 
 
+# TODO: replace flask's default http server from werkzeug to gunicorn
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 27017))
     app.run(host='0.0.0.0', port=port)
