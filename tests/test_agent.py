754566ea0081f36f1a07d77927aeb81e15d78efa
flyotlin <flyotlin@gmail.com>
2022-05-05 15:49:58 +0800

    Add unit test for BadmintonReserveAgent

--- tests/test_agent.py
+++ tests/test_agent.py
@@ -1,19 +1,36 @@
 import unittest
 
-from ..src.agent import Agent
+from src.agent import BadmintonReserveAgent
 
 
 class TestAgent(unittest.TestCase):
     def setUp(self) -> None:
-        self.agent = Agent()
-        self.agent.login()
+        _token = {
+            'PHPSESSID': '',
+            'XSRF-TOKEN': '',
+            '17fit_system_session': ''
+        }
+        self.agent = BadmintonReserveAgent(_token)
 
-    def test_login(self):
-        pass
+    @unittest.skip
+    def test_check(self):
+        expected_keys = ['court_idx', 'date', 'time']
 
-    def test_logout(self):
-        pass
+        court_datetime_list = self.agent.check("05-07", courts=(1,2,3,4,5,6))
+
+        self.assertIsInstance(court_datetime_list, list)
+        for i in court_datetime_list:
+            for j in expected_keys:
+                self.assertIn(j, i.keys())
+
+    def test_go(self):
+        _court = 3
+        _date = "05-09"
+        _time = "07:00"
+
+        ret = self.agent.go(court=_court, date=_date, time=_time)
+
+        self.assertTrue(ret)
 
     def tearDown(self) -> None:
-        self.agent.logout()
-        self.agent.bye()
+        return super().tearDown()
